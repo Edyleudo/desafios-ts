bf0ede08225086d54af4cdc1bf2f50d88c10f041
edyleudo <edyleudo@hotmail.com>
2022-05-31 10:00:22 -0300

    commit 2

--- desafio1.ts
+++ desafio1.ts
@@ -5,3 +5,21 @@ let employee = {};
 employee.code = 10;
 employee.name = "John";
 */
+
+// resposta 01
+const employee ={
+    codigo: 10,
+    nome: 'joão'
+};
+
+//resposta 02
+const employee2: {codigo: number, nome: string}={
+    codigo:10,
+    nome: 'joão'
+};
+
+//resposta 03
+interface employee {
+    codigo: number,
+    nome: string
+};
